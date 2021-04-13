8815e3b7104ae22dbd2f57f943d412243af52638
TowelMan-public <OdagiriTomoya@outlook.jp>
2021-04-13 13:48:53 +0900

    /web/user/confingページに関すること実装完了

---
+++ src/main/java/com/example/demo/controller/user/UserConfingControl.java
@@ -0,0 +1,74 @@
+package com.example.demo.controller.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.ModelSetter;
+import com.example.demo.UrlConfig;
+import com.example.demo.form.update.UpdatePassword;
+import com.example.demo.form.update.UpdateUsername;
+import com.example.demo.security.UserDetailsImp;
+import com.example.demo.service.UserDetailsServiceImp;
+
+@Controller
+@RequestMapping(UserConfingControl.URL)
+public class UserConfingControl {
+	public static final String URL = UrlConfig.ROOT_URL + "/user/confing";
+	@Autowired
+	UserDetailsServiceImp userDetailsService;
+	
+	@GetMapping
+	public String showPage(@AuthenticationPrincipal UserDetailsImp user, Model model) {
+		return new ModelSetter(model,ModelSetter.PAGE_USER_CONFING)
+				
+					.setUpdateUsername(
+							userDetailsService.getUsernameToUpdateUsernameForm(user))
+					
+					.setUpdatePassword(
+							new UpdatePassword())
+					
+					.buildAndReturnUrl();
+	}
+	
+	@PostMapping("/update/username")
+	public String updateUsername(@AuthenticationPrincipal UserDetailsImp user,
+			@Validated UpdateUsername form, BindingResult result, RedirectAttributes redirect) {
+		//入力ﾁｪｯｸ
+		if(result.hasErrors()) {
+			redirect.addFlashAttribute("org.springframework.validation.BindingResult.UpdateUsername", result);
+			redirect.addFlashAttribute("UpdateUsername", form);
+			return "redirect:" + URL;
+		}
+		
+		//処理
+		userDetailsService.updateUsername(user,form);
+		
+		//リダイレクト
+		return "redirect:" + URL;
+	}
+	
+	@PostMapping("/update/username")
+	public String updatePassword(@AuthenticationPrincipal UserDetailsImp user,
+			@Validated UpdatePassword form, BindingResult result, RedirectAttributes redirect) {
+		//入力ﾁｪｯｸ
+		if(result.hasErrors()) {
+			redirect.addFlashAttribute("org.springframework.validation.BindingResult.UpdatePassword", result);
+			redirect.addFlashAttribute("UpdatePassword", form);
+			return "redirect:" + URL;
+		}
+		
+		//処理
+		userDetailsService.updatePassword(user,form);
+		
+		//リダイレクト
+		return "redirect:" + URL;
+	}
+}
--- src/main/java/com/example/demo/service/UserDetailsServiceImp.java
+++ src/main/java/com/example/demo/service/UserDetailsServiceImp.java
@@ -5,6 +5,10 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.client.api.UserApi;
+import com.example.demo.client.api.entity.UserEntity;
+import com.example.demo.form.update.UpdatePassword;
+import com.example.demo.form.update.UpdateUsername;
+import com.example.demo.security.UserDetailsImp;
 
 @Service
 public class UserDetailsServiceImp{
@@ -15,4 +19,30 @@ public class UserDetailsServiceImp{
 		return userApi.login(username, password);
 	}
 
+	public void logout(UserDetailsImp user) {
+		userApi.logout(user);
+	}
+	
+	public void deleteUser(UserDetailsImp user) {
+		userApi.deleteUser(user);
+	}
+	
+	public UserEntity getUser(UserDetailsImp user) {
+		return userApi.getUser(user);
+	}
+
+	public UpdateUsername getUsernameToUpdateUsernameForm(UserDetailsImp user) {
+		return new UpdateUsername(
+					userApi.getUser(user)
+						.getUsername());
+	}
+
+	public void updateUsername(UserDetailsImp user, UpdateUsername form) {
+		userApi.updateUsername(user, form.getUsername());
+	}
+
+	public void updatePassword(UserDetailsImp user, UpdatePassword form) {
+		userApi.updatePassword(user, form.getNewPassword(), form.getOneMorePassword());
+	}
+	
 }
