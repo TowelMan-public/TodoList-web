f50cbb0cf46f795de9d245551460e025165c44b3
TowelMan-public <OdagiriTomoya@outlook.jp>
2021-05-07 22:41:10 +0900

    デバッグ　join/space/public

--- src/main/java/com/example/demo/form/insert/JoinPublicSpaceForm.java
+++ src/main/java/com/example/demo/form/insert/JoinPublicSpaceForm.java
@@ -1,12 +1,12 @@
 package com.example.demo.form.insert;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import com.example.demo.RegexpMessage;
 import lombok.Data;
 
 @Data
 public class JoinPublicSpaceForm {
-	@NotBlank(message=RegexpMessage.INTEGER)
+	@NotNull(message=RegexpMessage.INTEGER)
 	private Integer spaceId;
 }
